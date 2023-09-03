ea1d22de4b7f37cdb9aa8f6f0193789896d6a318
Brandon Pai <42866655+OkazakiBigHammer@users.noreply.github.com>
2023-09-03 14:53:25 +0800

    Update and rename README.md to index.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# me
-I want to try build my own webpage to show my life and my work.
---
+++ index.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Your Website</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#essays">Essays</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <h2>Welcome to My Website</h2>
+        <p>This is the home page of your website.</p>
+    </section>
+
+    <section id="essays">
+        <h2>Essays</h2>
+        <p>This is where you can list the essays you've read.</p>
+    </section>
+
+    <section id="projects">
+        <h2>Projects</h2>
+        <p>This is where you can showcase the projects you've worked on.</p>
+    </section>
+
+    <section id="about">
+        <h2>About Me</h2>
+        <p>This is where you can introduce yourself.</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Name. All rights reserved.</p>
+    </footer>
+
+    <script src="scripts.js"></script>
+</body>
+
+</html>
